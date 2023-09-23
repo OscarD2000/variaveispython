257fa351a2b2a0b97d4990dd01bdc435054e7a7d
OscarD2000 <145858784+OscarD2000@users.noreply.github.com>
2023-09-23 17:19:21 -0300

    Add files via upload

---
+++ AULA_2PYTHON.py
@@ -0,0 +1,34 @@
+# numero=2
+# numero2=4
+# print(numero+numero2)
+# cumprimento = 'Hello'
+# print(cumprimento)
+# boleano = True
+# boleano2 = False
+# print(boleano,boleano2)
+# real = 2.5
+# print(real)
+
+# nome = input('Digite o seu bone\n')
+
+
+# print("Calculadora de soma")
+# n1 = float(input("Digíte um número\n"))
+# n2 = float(input("Digíte o número 2\n"))
+# n3 = n1 - n2
+# print("O resultado da soma é", n3)
+
+# print("calculadora de soma")
+# n1 = float(input("Digite un número\n"))
+# n2 = float(input("Digite o número 5\n"))
+# n3 = n1 + n2
+# print("O resultado da soma é", n3) 
+
+
+
+# nome = input("Digite seu nome")
+# print("bom dia,",nome)
+
+
+
+
